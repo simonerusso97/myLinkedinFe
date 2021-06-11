d8bcb554cd82a4d07b3657c7a403e922f70380a8
Simone Russo <97.russo@gmail.com>
2021-06-11 19:35:58 +0200

    inizio manage structure

--- src/app/components/manage-structure/manage-structure.component.ts
+++ src/app/components/manage-structure/manage-structure.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import {Attribute} from '../../models/attribute';
+import {Structure} from '../../models/structure';
+import {AttributeService} from '../../services/attribute.service';
+import {ToastrService} from 'ngx-toastr';
+import { StructureService } from 'src/app/services/structure.service';
 
 @Component({
   selector: 'app-manage-structure',
@@ -7,9 +13,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageStructureComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: Router, private structureService: StructureService,  private toastr: ToastrService) { }
+
+  structure: Structure = {} as Structure;
+  showingStructureList: Structure[] = [];
+  structureList: Structure[] = [];
+
+  @Input() item: Structure = {} as Structure;
+
+
+
 
   ngOnInit(): void {
+    this.structureService.findAllStructure().subscribe(
+      response => {
+        this.structureList = response;
+        this.showingStructureList = response;
+      }
+    );
+
   }
 
+  goToNewStructure(): void {
+    this.route.navigateByUrl('admin/createStructure');
+  }
+  goToModifica(structure: Structure ): void{
+    this.route.navigateByUrl('admin/modifyStructure');
+    this.item.name = structure.name;
+    this.item.description = structure.description;
+
+
+  }
+
+  searchStructure(value: string): void {
+    this.showingStructureList = [];
+    if (value === ''){
+      this.showingStructureList = this.structureList;
+    }
+    this.structureList.find(item => {
+      if (item.name.toUpperCase() === value.toUpperCase()) {
+        this.showingStructureList.unshift(item);
+      }
+    });
+
+  }
+
+
+  delete(structure: Structure): void {
+    this.structureService.delete(structure).subscribe(
+      response => {
+        this.structureList = this.structureList.filter(item => item.id !== structure.id);
+        this.showingStructureList = this.structureList;
+      },
+      error => {
+        this.toastr.error('OPERAZIONE FALLITA');
+      });
+  }
 }
