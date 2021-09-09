417dafd8228a7884e95587fd082bcc4c5002da4b
Simone Russo <34507615+simonerusso97@users.noreply.github.com>
2021-09-09 19:30:31 +0200

    login html da sistemare

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { LoginComponent } from './components/login/login.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    RouterModule
 
   ],
   providers: [],
